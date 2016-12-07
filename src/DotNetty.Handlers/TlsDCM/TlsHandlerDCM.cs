b7859f8013a48a094ef00635e93ad5bc86b3c9d0
JD Marymee <jmarymee@microsoft.com>
2016-12-07 14:53:25 -0800

    Added new file for the new TLS Handler module for APQs DCM

---
+++ src/DotNetty.Handlers/TlsDCM/TlsHandlerDCM.cs
@@ -0,0 +1,26 @@
+﻿namespace DotNetty.Handlers.Tls
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Diagnostics.Contracts;
+    using System.IO;
+    using System.Net.Security;
+    using System.Runtime.ExceptionServices;
+    using System.Security.Cryptography.X509Certificates;
+    using System.Threading;
+    using System.Threading.Tasks;
+    using DotNetty.Buffers;
+    using DotNetty.Codecs;
+    using DotNetty.Common.Concurrency;
+    using DotNetty.Common.Utilities;
+    using DotNetty.Transport.Channels;
+
+    public sealed class TlsHandlerDCM : ByteToMessageDecoder
+    {
+        protected override void Decode(IChannelHandlerContext context, IByteBuffer input, List<object> output)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
+
